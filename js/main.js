a11585ed602662b354767887711545f915db0417
ilyalomaeff <94130262+ilyalomaeff@users.noreply.github.com>
2022-06-06 12:16:49 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -125,3 +125,127 @@ $('.skfo').on('mouseenter', function() {
 $('.skfo').on('mouseleave', function() {
     $('.map-table__skfo').removeClass('active');
 });
+
+
+$('.card path').on('mouseenter', function() {
+    $('.map-table').removeClass('active');
+});
+$('.card path').on('mouseleave', function() {
+    $('.map-table').addClass('active');
+});
+
+
+$('.pfo').on('mouseenter', function() {
+    $('.pfo-table').addClass('active');
+});
+$('.pfo').on('mouseleave', function() {
+    $('.pfo-table').removeClass('active');
+});
+
+
+$('.skfo').on('mouseenter', function() {
+    $('.skfo-table').addClass('active');
+});
+$('.skfo').on('mouseleave', function() {
+    $('.skfo-table').removeClass('active');
+});
+
+
+$('.cfo').on('mouseenter', function() {
+    $('.cfo-table').addClass('active');
+});
+$('.cfo').on('mouseleave', function() {
+    $('.cfo-table').removeClass('active');
+});
+
+
+$('.ufo').on('mouseenter', function() {
+    $('.ufo-table').addClass('active');
+});
+$('.ufo').on('mouseleave', function() {
+    $('.ufo-table').removeClass('active');
+});
+
+
+$('.pfo').on('mouseenter', function() {
+    $('.pfo-table').addClass('active');
+});
+$('.pfo').on('mouseleave', function() {
+    $('.pfo-table').removeClass('active');
+});
+
+
+$('.szfo').on('mouseenter', function() {
+    $('.szfo-table').addClass('active');
+});
+$('.szfo').on('mouseleave', function() {
+    $('.szfo-table').removeClass('active');
+});
+
+
+$('.uralfo').on('mouseenter', function() {
+    $('.uralfo-table').addClass('active');
+});
+$('.uralfo').on('mouseleave', function() {
+    $('.uralfo-table').removeClass('active');
+});
+
+
+$('.sfo').on('mouseenter', function() {
+    $('.sfo-table').addClass('active');
+});
+$('.sfo').on('mouseleave', function() {
+    $('.sfo-table').removeClass('active');
+});
+
+
+$('.dvfo').on('mouseenter', function() {
+    $('.dvfo-table').addClass('active');
+});
+$('.dvfo').on('mouseleave', function() {
+    $('.dvfo-table').removeClass('active');
+});
+
+$('.tab-buttons__item-1').on('click', function() {
+    $('.tab-buttons__item').removeClass('active');
+    $('.tab-content').removeClass('active');
+    $('.tab-buttons__item-1').addClass('active')
+    $('.tab-content-1').addClass('active');
+    $('.map-table').removeClass('table-act');
+    $('.map-table-1').addClass('table-act');
+    $('.single-table__item').removeClass('active');
+    $('.single-table__item:nth-child(1)').addClass('active');
+});
+
+$('.tab-buttons__item-2').on('click', function() {
+    $('.tab-buttons__item').removeClass('active');
+    $('.tab-content').removeClass('active');
+    $('.tab-buttons__item-2').addClass('active')
+    $('.tab-content-2').addClass('active');
+    $('.map-table').removeClass('table-act');
+    $('.map-table-2').addClass('table-act');
+    $('.single-table__item').removeClass('active');
+    $('.single-table__item:nth-child(2)').addClass('active');
+});
+
+$('.tab-buttons__item-3').on('click', function() {
+    $('.tab-buttons__item').removeClass('active');
+    $('.tab-content').removeClass('active');
+    $('.tab-buttons__item-3').addClass('active')
+    $('.tab-content-3').addClass('active');
+    $('.map-table').removeClass('table-act');
+    $('.map-table-3').addClass('table-act');
+    $('.single-table__item').removeClass('active');
+    $('.single-table__item:nth-child(3)').addClass('active');
+});
+
+$('.tab-buttons__item-4').on('click', function() {
+    $('.tab-buttons__item').removeClass('active');
+    $('.tab-content').removeClass('active');
+    $('.tab-buttons__item-4').addClass('active')
+    $('.tab-content-4').addClass('active');
+    $('.map-table').removeClass('table-act');
+    $('.map-table-4').addClass('table-act');
+    $('.single-table__item').removeClass('active');
+    $('.single-table__item:nth-child(4)').addClass('active');
+});
